9eb20982dca4463a1287c22567fb16ffc8523fef
Bhavya <bhavyaa192@gmail.com>
2026-09-07 15:27:31 +0530

    Task1.5

---
+++ Task1/Task1_5.py
@@ -0,0 +1,15 @@
+def fib(x):
+    if x == 0:
+         print(0)
+    elif x == 1:
+        print(1)
+    else:
+        a,b = 0,1
+        for i in range(2,x+1):
+            c = a + b
+            a = b
+            b = c
+        print(c)
+
+
+fib(int(input("Enter a number: ")))
